9158530e1dd26de1f5d801919a00590fcad56242
csamatov96 <996bkg@gmail.com>
2019-08-11 04:40:47 -0500

    userdata ""

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
     instance_type = "${var.instance_type}"
     key_name = "${var.key_name}"
     subnet_id = "${aws_subnet.public}" #on which subnet it'll live
-    user_data = "${file(userdata.sh)}" #${var.user_data}
+    user_data = "${file("userdata.sh")}" #${var.user_data}
     #count = "${var.count_ }"
     vpc_security_group_ids = ["${aws_security_group.public.id}"] #which security group it's going to be part of. FULL PATH
     associate_public_ip_address = "true" #so it'll be accessible 
