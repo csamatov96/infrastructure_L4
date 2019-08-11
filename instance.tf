397aea6d44a48cf6a524857fdd58cf75d6864825
csamatov96 <996bkg@gmail.com>
2019-08-11 04:26:56 -0500

    changes

--- instance.tf
+++ instance.tf
@@ -2,15 +2,15 @@ resource "aws_instance" "web" {
     ami = "${var.ami}"
     instance_type = "${var.instance_type}"
     key_name = "${var.key_name}"
-    subnet_id = "" #on which subnet it'll live
+    subnet_id = "${aws_subnet.public}" #on which subnet it'll live
     user_data = "${var.user_data}"
-    count = 
-    vpc_security_group_ids = "${}" #which security group it's going to be part of
+    count = "${var.count}"
+    vpc_security_group_ids = ["${aws_security_group.public.id}"] #which security group it's going to be part of. FULL PATH
     associate_public_ip_address = "true" #so it'll be accessible 
 
 
     tags = {
-      Name = "${var.Name}"
+      Name = "${var.Name}${count.index +1}" #
       Env = "${var.Env}"
       Created_by = "${var.Created_by}"
       Dept = "${var.Dept}"
