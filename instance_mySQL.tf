46d6d5db9697b46229905ebbeef43a98e528c7b9
csamatov96 <996bkg@gmail.com>
2019-08-11 04:57:40 -0500

    zxzx

--- instance_mySQL.tf
+++ instance_mySQL.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "DB" {
     instance_type = "${var.instance_type}"
     key_name = "${var.key_name}"
     subnet_id = "${aws_subnet.private}" #on which subnet it'll live
-    user_data = "${file("userdata.sh")}" #${var.user_data}
+    user_data = "${file("userdata_DB.sh")}" #${var.user_data}
     #count = "${var.count_ }"
     vpc_security_group_ids = ["${aws_security_group.private.id}"] #which security group it's going to be part of. FULL PATH
     associate_public_ip_address = "false" #so it'll be accessible 
