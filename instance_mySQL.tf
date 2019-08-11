8c92b2279b18d28a5107a0bfb1748d860051137a
csamatov96 <996bkg@gmail.com>
2019-08-11 05:14:27 -0500

    ID added in MYSQL instance subnet

--- instance_mySQL.tf
+++ instance_mySQL.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "DB" {
     ami = "${var.ami}"
     instance_type = "${var.instance_type}"
     key_name = "${var.key_name}"
-    subnet_id = "${aws_subnet.private}" #on which subnet it'll live
+    subnet_id = "${aws_subnet.private.id}" #on which subnet it'll live #ID ADDED
     user_data = "${file("userdata_DB.sh")}" #${var.user_data}
     #count = "${var.count_ }"
     vpc_security_group_ids = ["${aws_security_group.private.id}"] #which security group it's going to be part of. FULL PATH
