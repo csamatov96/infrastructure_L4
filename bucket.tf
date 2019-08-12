613220398d3832a03c262106062899da6a466b42
csamatov96 <996bkg@gmail.com>
2019-08-12 15:31:17 -0500

    2 bucket added

---
+++ bucket.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "bucket0" {
+    bucket = "infrastructure-L3-chyngyzkan0"
+}
---
+++ dontcreate.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "bucket1" {
+    bucket = "infrastructure-L3-chyngyzkan1"
+}
