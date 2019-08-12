82ceb167aca59ff594cc3bd78ad6098c7f2400b4
csamatov96 <996bkg@gmail.com>
2019-08-12 16:09:56 -0500

    module instance added

---
+++ module_instance.tf
@@ -0,0 +1,21 @@
+module "ec2_instance_byModule" {
+  source                 = "terraform-aws-modules/ec2-instance/aws" #pull the following from Terraform registry 
+  version                = "~> 3.0"
+
+  name                   = "ec2_instance_byModule"
+  instance_count         = 3 #
+
+  ami                    = "${var.ami}" #
+  instance_type          = "${var.instance_type}" #
+  key_name               = "${var.key_name}" #
+  monitoring             = true #
+  vpc_security_group_ids = ["${aws_security_group.public.id}"] #it'll be in public one
+  subnet_id              = "${aws_subnet.public.id}" #it'll be in public one
+
+  tags = {
+      Name = "${var.Name0}"
+      Env = "${var.Env}"
+      Created_by = "${var.Created_by}"
+      Dept = "${var.Dept}"
+  }
+}
