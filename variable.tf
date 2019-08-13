f2fbe854a5400c338f78fceecb903c919e8c0db9
csamatov96 <996bkg@gmail.com>
2019-08-12 22:23:52 -0500

    {} added

--- variable.tf
+++ variable.tf
@@ -14,10 +14,10 @@ variable "key_name" {}
 
 variable "app_name" {}
 
-variable "min_app_size"               
-variable "max_app_size"             
-variable "desired_app_capacity"   
+variable "min_app_size" {}               
+variable "max_app_size" {}             
+variable "desired_app_capacity" {}   
 
-variable "min_db_size"                  
-variable "max_db_size"                 
-variable "desired_db_capacity"          
+variable "min_db_size" {}                  
+variable "max_db_size" {}                 
+variable "desired_db_capacity" {}          
